4a4b566f77c8c1a91d5e2959ad7e3d405ac30b45
ramak8sgcp <ramak8sgcp@gmail.com>
2025-11-25 08:42:27 +0530

    update

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "81s-remote-state-dev-ram"
+    bucket = "81s-remote-state-rama"
     key    = "roboshop-manual"
     region = "us-east-1"
     dynamodb_table = "81s-locking"
